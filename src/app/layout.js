d9321c3a6d071beae1a86c80a035fff437129b47
Mahdi <mhdyrstmy1380@gmail.com>
2025-02-17 00:48:25 +0330

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body
-        style={{ fontFamily: "Digikala" }}
+        style={{ fontFamily: "Peyda" }}
         className="selection:bg-[#ff006e] selection:bg-opacity-50"
       >
         {children}
