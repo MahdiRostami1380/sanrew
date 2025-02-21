efebb1a9d6743532eabdbae2c06dc109371621e1
Mahdi <mhdyrstmy1380@gmail.com>
2025-02-21 22:48:34 +0330

    23

--- src/actions/FetchProducts.js
+++ src/actions/FetchProducts.js
@@ -2,34 +2,15 @@
 import { headers } from "next/headers";
 
 export async function fetchProducts() {
-  // const headersList = await headers();
-  // const host = headersList.get("X-Forwarded-Host");
-  // const proto = headersList.get("X-Forwarded-Proto");
+  const headersList = await headers();
+  const host = headersList.get("X-Forwarded-Host");
+  const proto = headersList.get("X-Forwarded-Proto");
 
-  // const response = await fetch(`${proto}://${host}/api/getProducts`, {
-  //   method: "GET",
-  // });
+  const response = await fetch(`${proto}://${host}/api/getProducts`, {
+    method: "GET",
+  });
 
-  // const products = await response.json();
+  const products = await response.json();
 
-  // console.log(products);
-
-  let x = [
-    {
-      _id: "67b8c5b5275dc282f92b9902",
-      name: "انگشتر",
-      price: 100,
-      id: "1244343",
-      details: [],
-      rating: 5,
-      description: "توضیحات محصول",
-      thumbnail: "/gol_sar.jpg",
-      images: ["/gol_sar.jpg"],
-      count: 100,
-      categories: [{ title: "fsf", value: "fs" }],
-      __v: 0,
-    },
-  ];
-
-  return x;
+  return products.products;
 }
