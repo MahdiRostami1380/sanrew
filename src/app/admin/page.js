962d6b277eff428640eacb89871552c888f2ec8e
Mahdi <mhdyrstmy1380@gmail.com>
2025-02-22 00:48:28 +0330

    345

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -2,6 +2,12 @@
 
 import { adminLogin } from "@/actions/AdminLogin";
 import { useState } from "react";
+import { GiPoliceOfficerHead } from "react-icons/gi";
+import { FaKey } from "react-icons/fa";
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
 
 const AdminPage = () => {
   const [usernamePassword, setUsernamePassword] = useState({
@@ -10,12 +16,18 @@ const AdminPage = () => {
   });
 
   return (
-    <main dir="ltr" className="mt-32 flex flex-col gap-5 items-center">
-      <div>
+    <main
+      dir="ltr"
+      className={`mt-32 flex flex-col gap-5 items-center ${inter.className}`}
+    >
+      <h1 className="font-bold text-3xl underline">Admin Login</h1>
+      <div className="flex items-center border-2 border-[#ff006e] rounded-xl p-3">
+        <GiPoliceOfficerHead className="text-2xl" />
         <input
           type="text"
           value={usernamePassword.username}
           placeholder="Username"
+          className="py-1 px-4 rounded-sm bg-transparent outline-none"
           onChange={(e) =>
             setUsernamePassword({
               ...usernamePassword,
@@ -24,11 +36,13 @@ const AdminPage = () => {
           }
         />
       </div>
-      <div>
+      <div className="flex items-center border-2 border-[#ff006e] rounded-xl p-3">
+        <FaKey className="text-2xl" />
         <input
-          type="text"
+          type="password"
           value={usernamePassword.password}
           placeholder="Password"
+          className="py-1 px-4 rounded-sm bg-transparent outline-none"
           onChange={(e) =>
             setUsernamePassword({
               ...usernamePassword,
@@ -37,7 +51,12 @@ const AdminPage = () => {
           }
         />
       </div>
-      <button onClick={() => adminLogin(usernamePassword)}>Login</button>
+      <button
+        onClick={() => adminLogin(usernamePassword)}
+        className="bg-slate-800 text-slate-50 px-4 py-2 rounded-xl"
+      >
+        Login
+      </button>
     </main>
   );
 };
