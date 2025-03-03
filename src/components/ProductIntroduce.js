46ead66cf1cbc61f58ad21603321c33bd8b298e9
Mahdi <mhdyrstmy1380@gmail.com>
2025-03-04 00:11:20 +0330

    ttt

--- src/components/ProductIntroduce.js
+++ src/components/ProductIntroduce.js
@@ -9,9 +9,12 @@ const ProductIntroduce = ({ product }) => {
         <img src={product.thumbnail || "/gol_sar.jpg"} alt={product.name} />
       </div>
       <div className="flex flex-col justify-between flex-1">
-        <div className="flex flex-col gap-3 md:gap-5 mb-3 md:mb-0">
+        <div
+          className="flex flex-col gap-3 md:gap-5 mb-3 md:mb-0"
+          style={{ fontFamily: "Peyda" }}
+        >
           <h1 className="text-3xl md:text-4xl lg:text-5xl">{product.name}</h1>
-          <div className="text-2xl" style={{ fontFamily: "Peyda" }}>
+          <div className="text-2xl">
             {product.caption.split(/\r\n|\n/).map((line, index) => (
               <p key={index}>{line}</p>
             ))}
